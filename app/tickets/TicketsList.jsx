c5a3c0229ed23a77bac0304d700929d6f1161309
(luqman syahreno) <eno.luqman@gmail.com>
2023-10-07 16:06:02 +0700

    Static Rendering

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -19,7 +19,7 @@ export default async function TicketsList() {
                 <div key={ticket.id} className="card my-5">
                     <Link href={`/tickets/${ticket.id}`}>
                         <h3>{ticket.title}</h3>
-                        <p>{ticket.body.slice(0, 200)}</p>
+                        <p>{ticket.body.slice(0, 200)}...</p>
                         <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
                     </Link>
                 </div>
